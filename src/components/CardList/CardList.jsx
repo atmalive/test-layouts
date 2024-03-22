babca0d27d355c2a538ac7382b06d650d128c779
mikhail <atmalive@gmail.com>
2024-03-22 11:47:06 +0100

    feat: final

--- src/components/CardList/CardList.jsx
+++ src/components/CardList/CardList.jsx
@@ -38,7 +38,7 @@ export const CardList = ({isVacancy = true}) => {
     }
 
     return (
-        <div className="flex justify-start items-center flex-wrap gap-3">
+        <div className="flex justify-center w-full md:justify-start items-center flex-wrap gap-5">
             <div className="flex flex-col items-start justify-start gap-6 max-w-[300px] min-w-72 w-full hover:opacity-90 cursor-pointer">
                 <img className="w-full max-h-36 object-cover rounded"
                      src="/4.png" alt="Копирайтер"/>
@@ -47,26 +47,27 @@ export const CardList = ({isVacancy = true}) => {
                 </div>
             </div>
             <div className="flex flex-col items-start justify-start gap-6 max-w-[300px] min-w-72 w-full hover:opacity-90 cursor-pointer">
-                <img className="w-full max-h-36 object-cover rounded"
-                     src="/3.png" alt="Копирайтер"/>
+                <img className="w-full max-h-36 object-cover  rounded"
+                     src="/1.png" alt="Копирайтер"/>
                 <div
                     className="text-center font-inter text-[23px] font-medium tracking-normal text-[#364670]">Механик резюме
                 </div>
             </div>
             <div className="flex flex-col items-start justify-start gap-6 max-w-[300px] min-w-72 w-full hover:opacity-90 cursor-pointer">
                 <img className="w-full max-h-36 object-cover rounded"
-                     src="/2.png" alt="Копирайтер"/>
+                     src="/3.png" alt="Копирайтер"/>
                 <div
                     className="text-center font-inter text-[23px] font-medium tracking-normal text-[#364670]">Шеф-повар резюме
                 </div>
             </div>
             <div className="flex flex-col items-start justify-start gap-6 max-w-[300px] min-w-72 w-full hover:opacity-90 cursor-pointer">
                 <img className="w-full max-h-36 object-cover rounded"
-                     src="/1.png" alt="Копирайтер"/>
+                     src="/2.png" alt="Копирайтер"/>
                 <div
                     className="text-center font-inter text-[23px] font-medium tracking-normal text-[#364670]">Адвокат резюме
                 </div>
             </div>
         </div>
     )
+
 }
